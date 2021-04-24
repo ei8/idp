8260e1bf5a304ff82431f8d96f31eef9a985c943
elmerbool <35117496+elmerbool@users.noreply.github.com>
2021-04-24 19:29:53 +1000

    Updated paths and names.

--- src/main/Constants.cs
+++ src/main/Constants.cs
@@ -17,7 +17,6 @@ namespace ei8.IdP
             public const string ClientsD23 = "CLIENTS_D23";
             public const string ClientsD23Secret = "CLIENTS_D23_SECRET";
             public const string ApisAvatarSecret = "APIS_AVATAR_SECRET";
-            public const string IssuerUri = "ISSUER_URI";
             public const string ConnectionStringsDefault = "CONNECTION_STRINGS_DEFAULT";
             public const string HostNameExpected = "HOST_NAME_EXPECTED";
             public const string HostNameReplacement = "HOST_NAME_REPLACEMENT";
